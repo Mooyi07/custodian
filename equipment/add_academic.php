3dd01ba565455b419bcf182ab8c4f12304d61448
Lanz Joseph Tarrosa Villanueva <120180277+Mooyi07@users.noreply.github.com>
2025-04-14 15:27:24 +0800

    Update add_academic.php
    Fixed the negative value of Quantity

--- equipment/add_academic.php
+++ equipment/add_academic.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label for="item_qty" class="form-label fw-bold">Quantity</label>
-                                <input type="number" class="form-control" id="item_qty" name="item_qty" placeholder="Enter Quantity" required>
+                                <input type="number" class="form-control" id="item_qty" name="item_qty" min=1 placeholder="Enter Quantity" required>
                             </div>
                         </div>
                         <div class="row">
